fd5af731ca5f243807cbb0cc89fb4dd052373875
cpustejovsky <charles.pustejovsky@gmail.com>
2019-06-25 10:27:19 -0400

    Changed secret page contet

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const indexRoutes = require("./routes/index.js");
 const port = (process.env.PORT = 3000);
 const localDb = "mongodb://localhost:27017/estuary";
 mongoose
-   .connect(localDb, { useNewUrlParser: true })
-   .then(() => console.log("connected to database"))
-   .catch(err => console.log(`ERROR: ${err}`));
+  .connect(localDb, { useNewUrlParser: true })
+  .then(() => console.log("connected to database"))
+  .catch(err => console.log(`ERROR: ${err}`));
 
 app.use(bodyParser.urlencoded({ extended: true })); // TODO: what does this mean?
 app.set("view engine", "ejs");
@@ -28,15 +28,15 @@ app.use(methodOverride("_method"));
 app.use(flash());
 // PASSPORT CONFIG
 app.use(
-   sessions({
-      cookieName: "session",
-      secret: "COME_BACK_TO_THIS_LATER",
-      duration: 30 * 60 * 1000, // 30 mins
-      activeDuration: 5 * 60 * 1000,
-      httpOnly: true, // don't let JS code access cookies
-      secure: true, // only set cookies over https
-      ephemeral: true // destroy cookies when the browser closes
-   })
+  sessions({
+    cookieName: "session",
+    secret: "COME_BACK_TO_THIS_LATER",
+    duration: 30 * 60 * 1000, // 30 mins
+    activeDuration: 5 * 60 * 1000,
+    httpOnly: true, // don't let JS code access cookies
+    secure: true, // only set cookies over https
+    ephemeral: true // destroy cookies when the browser closes
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -46,15 +46,15 @@ passport.deserializeUser(User.deserializeUser());
 
 //setting up res.locals
 app.use(function(req, res, next) {
-   res.locals.user = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   res.locals.ownership = req.flash("ownership");
-   next();
+  res.locals.user = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  res.locals.ownership = req.flash("ownership");
+  next();
 });
 
 app.use("/", indexRoutes);
 
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}!`);
+  console.log(`Example app listening on port ${port}!`);
 });
