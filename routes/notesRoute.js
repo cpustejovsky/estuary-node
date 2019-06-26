25f2d757f423d2fd03b8b231cfb45e1c1eae5658
cpustejovsky <charles.pustejovsky@gmail.com>
2019-06-26 14:25:32 -0400

    adding to new route

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -8,4 +8,12 @@ router.get("/", middleWare.isLoggedIn, (req, res) => {
   res.render("notes/index");
 });
 
+router.get("/new", middleWare.isLoggedIn, (req, res) => {
+  res.render("notes/new");
+});
+
+router.post("/", middleWare.isLoggedIn, (req, res) => {
+  res.send("successful post route (but nothing happened)");
+});
+
 module.exports = router;
