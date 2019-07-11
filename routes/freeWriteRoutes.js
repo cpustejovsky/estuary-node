5e3e38256cda2e445ce16b74998f633d040bd232
cpustejovsky <charles.pustejovsky@gmail.com>
2019-07-11 09:14:16 -0400

    forgot to save the delete route...

--- routes/freeWriteRoutes.js
+++ routes/freeWriteRoutes.js
@@ -34,14 +34,15 @@ router.post("/", middleWare.isLoggedIn, (req, res) => {
 router.delete("/:id", middleWare.isLoggedIn, (req, res) => {
   console.log("hit the delete route!");
   console.log(req.params.id);
-  for (let i = 0; i < req.user.freeWrites.length; i++) {
-    if (req.params.id === req.user.freeWrites._id) {
-      console.log(`
-      found a match!:
-      ${req.params.id} should match ${req.user.freeWrites._id}
-      `);
-    }
-  }
+  
+  // for (let i = 0; i < req.user.freeWrites.length; i++) {
+  //   if (req.params.id === req.user.freeWrites._id) {
+  //     console.log(`
+  //     found a match!:
+  //     ${req.params.id} should match ${req.user.freeWrites._id}
+  //     `);
+  //   }
+  // }
 });
 
 module.exports = router;
