8f7b784aae6d5100e46b05822d8be8c564ff2a71
cpustejovsky <charles.pustejovsky@gmail.com>
2019-09-05 08:13:19 -0400

    got delete route working for notes improved ui for notes and free writes inproved design for user page

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 // const passport = require("passport");
 // const moment = require("moment");
 const middleWare = require("../middleware/index.js");
+const User = require("../models/user");
 const Note = require("../schemas/noteSchema");
 // const FreeWriteChecker = require("../middleware/freeWriteChecker.js");
 
@@ -32,5 +33,23 @@ router.post("/", middleWare.isLoggedIn, (req, res) => {
 });
 
 //TODO: add delete route!
+router.delete("/:id", middleWare.isLoggedIn, (req, res) => {
+  console.log("hit the delete route!");
+  for (let i = 0; i < req.user.notes.length; i++) {
+    let notesId = req.user.notes[i]._id.toString();
+    if (req.params.id.toString() === notesId) {
+      console.log("YES match!");
+      User.findById(req.user._id.toString()).then(user => {
+        user.notes[i].remove();
+        user.save();
+      });
+      res.redirect("/notes");
+    } else {
+      console.log(`NO match!`);
+    }
+  }
+
+})
+
 
 module.exports = router;
