89723d2b52b14ef89f235a0408905eaf28fc4164
cpustejovsky <charles.pustejovsky@gmail.com>
2019-09-07 18:38:41 -0400

    adding email to user; need to fix that; also working with nodemailer

---
+++ middleware/mailer.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PW
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"NodeJS Application" donotreply@estuary.com',
+    to: "charles.pustejovsky@gmail.com",
+    subject: "Testing!",
+    html: <h1>Test Succeeded</h1>
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
+main().catch(console.error);
--- models/user.js
+++ models/user.js
@@ -9,6 +9,7 @@ const noteSchema = require("../schemas/noteSchema");
 const UserSchema = new Schema({
   username: String,
   password: String,
+  email: String,
   firstName: String,
   lastName: String,
   age: Number,
