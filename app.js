8888f7f77472cbec9e128185b1337e7edbf5fefd
cpustejovsky <charles.pustejovsky@gmail.com>
2019-09-08 10:58:21 -0400

    more debugging on prod! ugh!

--- app.js
+++ app.js
@@ -34,34 +34,35 @@ var j = schedule.scheduleJob(rule, function() {
   User.find({ emailUpdates: true }).then(users => {
     for (let i = 0; i < users.length; i++) {
       console.log(`found user! Here is their username \n ${users[i].username}`);
-      // let emailAddress = users[i].email;
-      // let noteContents = "<h1>Your Notes</h1>\n<ul>";
-      // for (let i = 0; i < users[i].notes.length; i++) {
-      //   noteContents += `\n<li>${users[i].notes[i].content}</li>`;
-      //   if (users[i].notes[i] === users[i].notes.length - 1) {
-      //     noteContents += "</ul>";
-      //   }
-      // }
-      // let emailContent = noteContents;
-      // async function emailUser(emailAddr) {
-      //   let transporter = nodemailer.createTransport({
-      //     service: "gmail",
-      //     auth: {
-      //       user: process.env.EMAIL,
-      //       pass: process.env.EMAIL_PW
-      //     }
-      //   });
-      //   // send mail with defined transport object
-      //   let info = await transporter.sendMail({
-      //     from: '"NodeJS Application" donotreply@estuary.com',
-      //     to: emailAddr,
-      //     subject: `${users[i].firstName} ${users[i].lastName}'s Notes`,
-      //     html: emailContent
-      //   });
-      // }
-      // emailUser(emailAddress);
-    }
-  });
+      User.findOne({ username: users[i].username }).then((user)=> {
+      let emailAddress = user.email;
+      let noteContents = "<h1>Your Notes</h1>\n<ul>";
+      for (let i = 0; i < user.notes.length; i++) {
+        noteContents += `\n<li>${user.notes[i].content}</li>`;
+        if (user.notes[i] === user.notes.length - 1) {
+          noteContents += "</ul>";
+        }
+      }
+      let emailContent = noteContents;
+      async function emailUser(emailAddr) {
+        let transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PW
+          }
+        });
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+          from: '"NodeJS Application" donotreply@estuary.com',
+          to: emailAddr,
+          subject: `${user.firstName} ${user.lastName}'s Notes`,
+          html: emailContent
+        });
+      }
+      emailUser(emailAddress);
+      })
+  };
 });
 
 if (process.argv[2] === "test") {
