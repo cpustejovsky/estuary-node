7f677efa4031ee1436fa86ffb7d672e70c76dfc0
cpustejovsky <charles.pustejovsky@gmail.com>
2019-10-09 19:41:47 -0400

    adding heroku ssl fixer to make sure estuary.cpustejovsky.com works

---
+++ middleware/herokuSSL.js
@@ -0,0 +1,17 @@
+//https://www.npmjs.com/package/heroku-ssl-redirect
+
+module.exports = function(environments, status) {
+  environments = environments || ["production"];
+  status = status || 302;
+  return function(req, res, next) {
+    if (environments.indexOf(process.env.NODE_ENV) >= 0) {
+      if (req.headers["x-forwarded-proto"] != "https") {
+        res.redirect(status, "https://" + req.hostname + req.originalUrl);
+      } else {
+        next();
+      }
+    } else {
+      next();
+    }
+  };
+};
