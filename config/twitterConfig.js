3aa23747bdc956063418ca85a03e8eb246eea4be
cpustejovsky <charles.pustejovsky@gmail.com>
2019-11-09 16:37:14 -0500

    incorporating twitter bot as module on estuary.

---
+++ config/twitterConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key: process.env.C0NSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
