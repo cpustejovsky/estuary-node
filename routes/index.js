7a62d2ebd0c90714abc4aae06fac119f19a4f000
cpustejovsky <charles.pustejovsky@gmail.com>
2019-12-06 20:06:48 -0500

    setting up boilerplate for password reset

--- routes/index.js
+++ routes/index.js
@@ -7,88 +7,96 @@ const middleWare = require("../middleware/index.js");
 // USER ROUTES
 
 router.get("/user", middleWare.isLoggedIn, (req, res) =>
-  res.render("user/index")
+    res.render("user/index")
 );
 
 router.get("/user/edit", middleWare.isLoggedIn, (req, res) => {
-  res.render("user/edit");
+    res.render("user/edit");
 });
 
 router.put("/user", middleWare.isLoggedIn, (req, res) => {
-  let updatedUser = {};
-  updatedUser.firstName = req.body.user.firstName;
-  updatedUser.lastName = req.body.user.lastName;
-  updatedUser.email = req.body.user.email;
-  updatedUser.age = req.body.user.age;
-  updatedUser.emailUpdates = req.body.user.emailUpdates;
-  User.findByIdAndUpdate(req.user.id, updatedUser).then(() => {
-    res.redirect("/user");
-  });
+    let updatedUser = {};
+    updatedUser.firstName = req.body.user.firstName;
+    updatedUser.lastName = req.body.user.lastName;
+    updatedUser.email = req.body.user.email;
+    updatedUser.age = req.body.user.age;
+    updatedUser.emailUpdates = req.body.user.emailUpdates;
+    User.findByIdAndUpdate(req.user.id, updatedUser).then(() => {
+        res.redirect("/user");
+    });
 });
 
 router.delete("/user", (req, res) => {
-  User.findByIdAndDelete(req.user.id).then(() => {
-    req.flash("success", "User deleted");
-    res.redirect("/");
-  });
+    User.findByIdAndDelete(req.user.id).then(() => {
+        req.flash("success", "User deleted");
+        res.redirect("/");
+    });
 });
 
 // INDEX ROUTES
 router.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
 });
 
 router.get("/about", (req, res) => {
-  res.render("about");
+    res.render("about");
 });
 
-//show login form
 router.get("/login", (req, res) => {
-  res.render("login");
+    res.render("login");
 });
-//show register form
+
 router.get("/register", (req, res) => {
-  res.render("register");
+    res.render("register");
 });
+
+router.get("/password-reset", (req, res) => {
+    res.render("password-reset")
+});
+
 //handle login logic
 router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/user",
-    failureRedirect: "/login",
-    failureFlash: "Invalid username or password",
-    successFlash: `Howdy! Welcome back!`
-  }),
-  (req, res) => {
-    //currently nothing?
-  }
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/user",
+        failureRedirect: "/login",
+        failureFlash: "Invalid username or password",
+        successFlash: `Howdy! Welcome back!`
+    }),
+    (req, res) => {
+        //currently nothing?
+    }
 );
 
 //handle register logic
 router.post("/register", (req, res) => {
-  var newUser = new User({ username: req.body.username });
-  User.register(newUser, req.body.password, function(err, user) {
-    if (err) {
-      req.flash("error", `Oops! ${err.message}`);
-      res.redirect("/register");
-    }
-    passport.authenticate("local")(req, res, () => {
-      user.firstName = req.body.user.firstName;
-      user.lastName = req.body.user.lastName;
-      user.email = req.body.user.email;
-      user.age = req.body.user.age;
-      user.save();
-      // console.log(user);
-      res.redirect("/user");
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            req.flash("error", `Oops! ${err.message}`);
+            res.redirect("/register");
+        }
+        passport.authenticate("local")(req, res, () => {
+            user.firstName = req.body.user.firstName;
+            user.lastName = req.body.user.lastName;
+            user.email = req.body.user.email;
+            user.age = req.body.user.age;
+            user.save();
+            // console.log(user);
+            res.redirect("/user");
+        });
     });
-  });
+});
+
+router.post("/password-reset", (req, res) => {
+    res.send("You tried to reset your password! Unfortunately, this functionality is still WIP");
 });
 
 //logout route and logic
 router.get("/logout", (req, res) => {
-  req.logout();
-  req.flash("success", "Logged out");
-  res.redirect("/");
+    req.logout();
+    req.flash("success", "Logged out");
+    res.redirect("/");
 });
 
 module.exports = router;
