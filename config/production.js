94e1772671760cf1e5e81a5a0b919cbbcbd8c063
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-25 19:35:49 -0400

    adding github oauth info to production

--- config/production.js
+++ config/production.js
@@ -1,6 +1,8 @@
 module.exports = {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   MONGODB_URL: process.env.MONGODB_URL,
   COOKIE_KEY: process.env.COOKIE_KEY,
   redirectDomain: process.env.redirectDomain
