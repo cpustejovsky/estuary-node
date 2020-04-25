f9d23f14142855a013756999bc5ee1793310a627
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-25 13:34:23 -0400

    notes creation component

---
+++ client/src/components/NotesNew.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+// import _ from "lodash";
+import { Formik } from "formik";
+import { connect } from "react-redux";
+import { createNote } from "../actions";
+class NotesNew extends Component {
+  submitValues(values) {
+    let history = this.props.history;
+    this.props.createNote(values, history);
+  }
+  render() {
+    console.log(this.props.history);
+    return (
+      <div>
+        <Formik
+          initialValues={{ content: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              this.submitValues(values);
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="button button__notes">
+                <h4 className="button__text__left">Create New Note</h4>
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="btn btn-lg margin-top"
+                >
+                  Save
+                </button>
+              </div>
+              <div className="input-field">
+                <div className="row">
+                  <div className="row">
+                    <div className="input-field col s12">
+                      <textarea
+                        name="content"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.content}
+                      ></textarea>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ auth, user }) => {
+  return { auth, user };
+};
+
+export default connect(mapStateToProps, { createNote })(NotesNew);
