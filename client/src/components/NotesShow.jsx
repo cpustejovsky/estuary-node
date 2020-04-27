4cdb6468240de9551635260ee7118ceb6de250f2
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-27 10:43:26 -0400

    fixed notes show page

--- client/src/components/NotesShow.jsx
+++ client/src/components/NotesShow.jsx
@@ -2,25 +2,26 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchNotes } from "../actions";
-import Loader from "./Loader"
+import Loader from "./Loader";
 class NotesShow extends Component {
   componentDidMount() {
     this.props.fetchNotes();
   }
-  renderRedirect(){
-  }
   renderNotes() {
-    return this.props.notes.map(({ content, _id }) => {
-      return (
-        <div key={_id} className="card darken-1">
-          <div className="card-content">
-            <p>{content}</p>
+    if (this.props.notes) {
+      return this.props.notes.map(({ content, _id }) => {
+        return (
+          <div key={_id} className="card darken-1">
+            <div className="card-content">
+              <p>{content}</p>
+            </div>
           </div>
-        </div>
-      );
-    });
+        );
+      });
+    }
   }
   render() {
+    console.log(this.props.notes);
     //TODO: what is a good way to deal with auth redirects?
     if (this.props.auth || this.props.user) {
       return (
@@ -35,11 +36,9 @@ class NotesShow extends Component {
         </div>
       );
     } else if (this.props.auth === null && this.props.user === null) {
-      return (
-        <Loader/>
-      );
-    } else if (!this.props.auth && !this.props.auth){
-      return <>{this.props.history.push("/auth")}</>
+      return <Loader />;
+    } else if (!this.props.auth && !this.props.auth) {
+      return <>{this.props.history.push("/auth")}</>;
     }
   }
 }
