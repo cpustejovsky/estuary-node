4f92d194c2427ae8306bae014f3473b3cda102c2
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-27 09:50:21 -0400

    mongo and express changes are working

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const FreeWriteSchema = require("../schemas/freeWriteSchema");
-const noteSchema = require("../schemas/noteSchema");
+const NoteSchema = require("../schemas/NoteSchema");
 
 //set up a Schema
 const UserSchema = new Schema({
@@ -15,7 +15,7 @@ const UserSchema = new Schema({
   age: Number,
   emailUpdates: { type: Boolean, default: false },
   freeWrites: [FreeWriteSchema],
-  notes: [noteSchema],
+  notes: [NoteSchema],
 });
 
 mongoose.model("users", UserSchema);
