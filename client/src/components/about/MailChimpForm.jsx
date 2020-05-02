16d172adfc6c692e89a2bd2129ba91333364efab
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-02 18:15:56 -0400

    got enough of input working to encounter a new error message

--- client/src/components/about/MailChimpForm.jsx
+++ client/src/components/about/MailChimpForm.jsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { Button, Input, TextField, FormControl } from "@material-ui/core";
 
 const url = process.env.REACT_APP_MC_URL;
 
-const CustomForm = ({ status, message, onValidated }) => {
-  let email;
-  const submit = () =>
-    email &&
-    email.value.indexOf("@") > -1 &&
-    onValidated({
-      EMAIL: email.value,
-    });
-
+const CustomForm = ({ status, message, onValidated, refs }) => {
+  const [email, setEmail] = useState("");
+  const submit = () => {
+    return email &&
+      email.indexOf("@") > -1 &&
+      onValidated({
+        EMAIL: email,
+      });
+  };
   return (
     <>
-      <div className="input-field">
-        <input
+      <FormControl fullWidth>
+        <TextField
+          fullWidth
+          label="Email Address"
+          variant="outlined"
           id="email"
           className="validate"
-          ref={(node) => (email = node)}
+          // ref={(node) => (email = node)}
           type="email"
-          placeholder="Your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         {status === "sending" && (
           <label htmlFor="email" style={{ color: "blue" }}>
@@ -43,10 +48,10 @@ const CustomForm = ({ status, message, onValidated }) => {
             dangerouslySetInnerHTML={{ __html: message }}
           />
         )}
-      </div>
-      <button className="btn" onClick={submit}>
-        Submit<i className="material-icons right">email</i>
-      </button>
+        <Button variant="outlined" color="primary" onClick={submit}>
+          Submit<i className="material-icons right">email</i>
+        </Button>
+      </FormControl>
     </>
   );
 };
@@ -54,7 +59,7 @@ const CustomForm = ({ status, message, onValidated }) => {
 const MailChimpForm = () => {
   return (
     <MailchimpSubscribe
-      url={url}
+      url={process.env.REACT_APP_MC_URL}
       render={({ subscribe, status, message }) => (
         <CustomForm
           status={status}
