4e9711aa5597b3cb7ce227647daa08e966a44267
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-03 18:45:43 -0400

    forgot to add default to Warning func comp

--- client/src/components/partials/Warning.jsx
+++ client/src/components/partials/Warning.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export function Warning() {
+export default function Warning() {
     return (
       <div className="site__warning">
         <strong>WARNING:</strong> This site is undergoing maintenance and is not production ready. Do not save essential data that you cannot retrieve.
