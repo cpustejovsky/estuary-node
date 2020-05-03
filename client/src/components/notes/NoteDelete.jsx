734917b89cc2d862cb1e01f379d135b2b7751964
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-03 15:35:41 -0400

    note delete func comp

--- client/src/components/notes/NoteDelete.jsx
+++ client/src/components/notes/NoteDelete.jsx
@@ -1,40 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { deleteNote } from "../../actions";
 import { Button, CardActions } from "@material-ui/core";
 import { green, red, grey } from "@material-ui/core/colors";
 
-class NoteDelete extends Component {
-  render() {
-    return (
-      <>
-        <CardActions>
-          <Button disabled style={{ color: grey[500] }}>
-            Are you sure?
-          </Button>
-        </CardActions>
-        <CardActions>
-          <Button
-            style={{ color: green[500] }}
-            onClick={() => {
-              this.props.deleteNote(this.props.id);
-              this.props.toggleDelete();
-            }}
-            className="click"
-          >
-            Yes
-          </Button>
-          <Button
-            style={{ color: red[500] }}
-            onClick={() => this.props.toggleDelete()}
-            className="click"
-          >
-            No
-          </Button>
-        </CardActions>
-      </>
-    );
-  }
+function NoteDelete(props) {
+  return (
+    <>
+      <CardActions>
+        <Button disabled style={{ color: grey[500] }}>
+          Are you sure?
+        </Button>
+      </CardActions>
+      <CardActions>
+        <Button
+          style={{ color: green[500] }}
+          onClick={() => {
+            props.deleteNote(props.id);
+            props.toggleDelete();
+          }}
+          className="click"
+        >
+          Yes
+        </Button>
+        <Button
+          style={{ color: red[500] }}
+          onClick={() => props.toggleDelete()}
+          className="click"
+        >
+          No
+        </Button>
+      </CardActions>
+    </>
+  );
 }
 
 export default connect(null, { deleteNote })(NoteDelete);
