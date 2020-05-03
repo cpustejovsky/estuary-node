d2d46b4930f9222aaab769a9796110524d574113
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-03 09:20:15 -0400

    free write show now func comp

--- client/src/components/freewrites/FreeWritesShow.jsx
+++ client/src/components/freewrites/FreeWritesShow.jsx
@@ -1,17 +1,21 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { fetchFreeWrites } from "../../actions";
 import Loader from "../partials/Loader";
 import { Button, Card, CardContent, Typography } from "@material-ui/core";
 
-class FreeWriteShow extends Component {
-  componentDidMount() {
-    this.props.fetchFreeWrites();
-  }
-  renderFreeWrites() {
-    if (this.props.freeWrites.length > 0) {
-      return this.props.freeWrites.map(({ content, _id, title }) => {
+function FreeWriteShow({ fetchFreeWrites, history }) {
+  const auth = useSelector((state) => state.auth);
+  const user = useSelector((state) => state.user);
+  const notes = useSelector((state) => state.notes);
+  const freeWrites = useSelector((state) => state.freeWrites);
+  useEffect(() => {
+    fetchFreeWrites();
+  });
+  const renderFreeWrites = () => {
+    if (freeWrites.length > 0) {
+      return freeWrites.map(({ content, _id, title }) => {
         return (
           <Card key={_id} raised className="margin-top">
             <CardContent>
@@ -26,40 +30,34 @@ class FreeWriteShow extends Component {
         );
       });
     }
-  }
-  render() {
-    console.log(this.props.freeWrites);
-    //TODO: what is a good way to deal with auth redirects?
-    if (this.props.auth || this.props.user) {
-      return (
-        <div>
-          <div className="button button__free-writes">
-            <Typography variant="h4" className="button__text__left">
-              Your Free Writes
-            </Typography>
-            <Button
-              component={RouterLink}
-              to="/free-writes/new"
-              size="large"
-              variant="contained"
-              color="primary"
-            >
-              New Free Write
-            </Button>
-          </div>
-          {this.renderFreeWrites()}
+  };
+
+  //TODO: what is a good way to deal with auth redirects?
+  if (auth || user) {
+    return (
+      <div>
+        <div className="button button__free-writes">
+          <Typography variant="h4" className="button__text__left">
+            Your Free Writes
+          </Typography>
+          <Button
+            component={RouterLink}
+            to="/free-writes/new"
+            size="large"
+            variant="contained"
+            color="primary"
+          >
+            New Free Write
+          </Button>
         </div>
-      );
-    } else if (this.props.auth === null && this.props.user === null) {
-      return <Loader />;
-    } else if (!this.props.auth && !this.props.auth) {
-      return <>{this.props.history.push("/login")}</>;
-    }
+        {renderFreeWrites()}
+      </div>
+    );
+  } else if (auth === null && user === null) {
+    return <Loader />;
+  } else if (!auth && !auth) {
+    return <>{history.push("/login")}</>;
   }
 }
 
-const mapStateToProps = ({ auth, user, notes, freeWrites }) => {
-  return { auth, user, notes, freeWrites };
-};
-
-export default connect(mapStateToProps, { fetchFreeWrites })(FreeWriteShow);
+export default connect(null, { fetchFreeWrites })(FreeWriteShow);
