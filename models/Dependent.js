afc855656f58de7320e9d680efe132eadb88e7fa
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-05 09:52:36 -0400

    default to recipient description and email of null in case user chooses to leave these blank

--- models/Dependent.js
+++ models/Dependent.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const DependentSchema = new Schema({
   name: String,
-  description: String,
-  email: String,
+  description: { type: String, default: null },
+  email: { type: String, default: null },
 });
 
-module.exports = DependentSchema
+module.exports = DependentSchema;
