7f9c53b2ae8b75311902be1e82be15778e7766e2
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-08 14:21:47 -0400

    set up delete for notactionable

--- client/src/components/notes/organize/NotActionable.jsx
+++ client/src/components/notes/organize/NotActionable.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 
-export default function NotActionable({ show, noteId, categorizeNote, deleteNote }) {
+export default function NotActionable({
+  show,
+  noteId,
+  categorizeNote,
+  deleteNote,
+  toggleActionable,
+  toggleNotActionable,
+}) {
   if (show) {
     return (
       <>
-      {/* 
+        {/* 
       TODO: delete notes when user clicks Trash
       TODO: send notes to maybe when user clicks Someday/Maybe
       TODO: send notes to reference when user clicks Reference
       */}
         <h2>Then what is it?</h2>
-        <Button onClick={()=>deleteNote(noteId)}>Trash</Button>
+        <Button
+          onClick={() => {
+            deleteNote(noteId);
+            toggleActionable();
+            toggleActionable();
+          }}
+        >
+          Trash
+        </Button>
         <Button>Someday/Maybe</Button>
         <Button>Reference</Button>
       </>
