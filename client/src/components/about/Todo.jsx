25bc231227e095dd11e8c8d594c5be0a95909dac
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-08 11:03:00 -0400

    set up subotdos on todos page; added and expanded upon todos;

--- client/src/components/about/Todo.jsx
+++ client/src/components/about/Todo.jsx
@@ -3,20 +3,21 @@ import ListItem from "@material-ui/core/ListItem";
 import { Check, CropSquare } from "@material-ui/icons";
 import { green, grey } from "@material-ui/core/colors";
 
-const Todo = ({ complete, text }) => {
+const Todo = ({ complete, text, subtodo }) => {
   if (complete) {
     return (
-      <ListItem className="valign-wrapper">
+      <ListItem
+        className={subtodo ? "valign-wrapper subtodo" : "valign-wrapper"}
+      >
         <Check style={{ color: green[500] }} />
         {text}
       </ListItem>
     );
   } else {
     return (
-      <ListItem className="valign-wrapper">
-        {/* <i className="material-icons grey-text" style={{ fontSize: "" }}>
-          crop_square
-        </i>{" "} */}
+      <ListItem
+        className={subtodo ? "valign-wrapper subtodo" : "valign-wrapper"}
+      >
         <CropSquare style={{ color: grey[500] }} />
         {text}
       </ListItem>
