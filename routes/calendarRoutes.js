7323b08d24e66552b3d5afc8a2aac7494c229837
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-19 09:33:06 -0400

    setting up google calendar post route

--- routes/calendarRoutes.js
+++ routes/calendarRoutes.js
@@ -2,7 +2,7 @@ const { google } = require("googleapis");
 const keys = require("../config/keys");
 
 module.exports = (app) => {
-  app.get("/api/calendar", async (req, res) => {
+  app.post("/api/calendar", async (req, res) => {
     const auth = new google.auth.OAuth2(
       keys.GOOGLE_CLIENT_ID,
       keys.GOOGLE_CLIENT_SECRET,
@@ -13,18 +13,28 @@ module.exports = (app) => {
       version: "v3",
       auth,
     });
+    var event = {
+      summary: req.body.title || "Title Goes Here",
+      location: req.body.location,
+      description: req.body.description || "Description Goes Here",
+      start: {
+        dateTime: req.body.startDate || new Date(),
+        timeZone: "America/New_York",
+      },
+      end: {
+        dateTime: req.body.endDate || new Date(),
+        timeZone: "America/New_York",
+      },
+    };
     try {
-      const events = await calendar.events.list({
+      const createdEvent = await calendar.events.insert({
+        auth: auth,
         calendarId: "primary",
-        timeMin: new Date().toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: "startTime",
+        resource: event,
       });
-      res.send(events);
+      res.send(createdEvent);
     } catch (error) {
-      console.log(`The API returned an error:\n${error}`);
-      res.send(`The API returned an error:\n${error}`);
+      res.send(error);
     }
   });
 };
