b7a7e71b8ceda18b57b82ee29c7e8b17b7be6dd9
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-20 11:32:59 -0400

    remove reselect import (not needed currently)

--- client/src/components/notes/NotesShow.jsx
+++ client/src/components/notes/NotesShow.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { connect, useSelector } from "react-redux";
-import { createSelector } from "reselect";
 import _ from "lodash";
 import { fetchNotesByCategory } from "../../actions";
 import Loader from "../partials/Loader";
