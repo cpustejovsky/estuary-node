6eaea43a8b69754db17e207736ef1e7e8f5e1149
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-22 10:23:10 -0400

    setting color to be white on login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -12,6 +12,7 @@ export default function Login() {
           component={Link}
           href="/auth/google"
           variant="contained"
+          style={{color: "white"}}
           color="primary"
           size="large"
         >
