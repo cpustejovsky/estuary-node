28479b8a3606f0e51e03c42bf1c895611c6b6586
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-23 20:48:50 -0400

    create note controllers

---
+++ controllers/noteController.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+const Note = mongoose.model("notes");
+
+module.exports = {
+  async findNotesByCategory(req, res) {
+    const userCategoryNotes = await Note.find({
+      _user: req.user.id,
+      category: req.params.name,
+    });
+    res.send(userCategoryNotes);
+  },
+  async findNotesByProject(req, res) {
+    const userProjectNotes = await Note.find({
+      _user: req.user.id,
+      _project: req.params.id,
+    });
+    res.send(userProjectNotes);
+  },
+  async createNote(req, res) {
+    const newNote = new Note({
+      content: req.body.content,
+      _user: req.user.id,
+      // tags: req.body.tags,
+    });
+    const response = await newNote.save();
+    res.send(response);
+  },
+  //TODO: is this the most efficient way to do update and destroy?
+  async updateNote(req, res) {
+    const updatedNote = await Note.findOneAndUpdate(
+      { _user: req.user.id, _id: req.body.noteId },
+      { content: req.body.content },
+      { new: true }
+    );
+    const response = await updatedNote.save();
+    res.send(response);
+  },
+  async updateNoteCategory(req, res) {
+    let updatedNote = await Note.findOneAndUpdate(
+      { _user: req.user.id, _id: req.body.noteId },
+      { _project: req.body.projectId, category: "project" },
+      { new: true }
+    );
+    response = await updatedNote.save();
+    res.send(response);
+  },
+  async attachNoteToProject(req, res) {
+    let category = req.params.category;
+    let updatedData = {
+      category: category.toLowerCase(),
+    };
+    let response;
+    switch (category.toLowerCase()) {
+      case "done":
+        updatedData.completed = true;
+        updatedData.completedDate = new Date();
+        break;
+      case "waiting":
+        updatedData.dependsOn = req.body.dependsOn || null;
+        break;
+      default:
+        break;
+    }
+    let updatedNote = await Note.findOneAndUpdate(
+      { _user: req.user.id, _id: req.body.noteId },
+      updatedData,
+      { new: true }
+    );
+    response = await updatedNote.save();
+    res.send(response);
+  },
+  async deleteNote(req, res) {
+    await Note.findOneAndDelete({ _user: req.user.id, _id: req.body.noteId });
+    res.send({});
+  },
+};
--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,83 +1,28 @@
-const mongoose = require("mongoose");
 const requireLogin = require("../middleware/requireLogin");
-const Note = mongoose.model("notes");
+const noteController = require("../controllers/noteController");
 module.exports = (app) => {
-  app.get("/api/notes/category/:name", requireLogin, async (req, res) => {
-    const userProjectNotes = await Note.find({
-      _user: req.user.id,
-      category: req.params.name,
-    });
-    res.send(userProjectNotes);
-  });
+  app.get(
+    "/api/notes/category/:name",
+    requireLogin,
+    noteController.findNotesByCategory
+  );
+  app.get(
+    "/api/notes/project/:id",
+    requireLogin,
+    noteController.findNotesByProject
+  );
+  app.post("/api/notes", requireLogin, noteController.createNote);
+  app.patch("/api/notes", requireLogin, noteController.updateNoteCategory);
+  app.patch(
+    "/api/notes/project",
+    requireLogin,
+    noteController.attachNoteToProject
+  );
 
-  app.get("/api/notes/project/:id", requireLogin, async (req, res) => {
-    const userProjectNotes = await Note.find({
-      _user: req.user.id,
-      _project: req.params.id,
-    });
-    res.send(userProjectNotes);
-  });
-
-  app.post("/api/notes", requireLogin, async (req, res) => {
-    const newNote = new Note({
-      content: req.body.content,
-      _user: req.user.id,
-      // tags: req.body.tags,
-    });
-    const response = await newNote.save();
-    res.send(response);
-  });
-  //TODO: is this the most efficient way to do update and destroy?
-  //await axios.put("/api/notes", {noteId: "5ea6e09ea4e289a7a45e36e3",content: "updated content again!"})
-  app.patch("/api/notes", requireLogin, async (req, res) => {
-    const updatedNote = await Note.findOneAndUpdate(
-      { _user: req.user.id, _id: req.body.noteId },
-      { content: req.body.content },
-      { new: true }
-    );
-    const response = await updatedNote.save();
-    res.send(response);
-  });
-
-  app.patch("/api/notes/project", requireLogin, async (req, res) => {
-    let updatedNote = await Note.findOneAndUpdate(
-      { _user: req.user.id, _id: req.body.noteId },
-      { _project: req.body.projectId, category: "project" },
-      { new: true }
-    );
-    response = await updatedNote.save();
-    res.send(response);
-  });
-
-  app.patch("/api/notes/:category", requireLogin, async (req, res) => {
-    let category = req.params.category;
-    let updatedData = {
-      category: category.toLowerCase(),
-    };
-    let response;
-    switch (category.toLowerCase()) {
-      case "done":
-        updatedData.completed = true;
-        updatedData.completedDate = new Date();
-        break;
-      case "waiting":
-        updatedData.dependsOn = req.body.dependsOn || null;
-        break;
-      default:
-        break;
-    }
-    let updatedNote = await Note.findOneAndUpdate(
-      { _user: req.user.id, _id: req.body.noteId },
-      updatedData,
-      { new: true }
-    );
-    response = await updatedNote.save();
-    res.send(response);
-  });
-
-  //await axios.delete("/api/notes", {data: {noteId: "5ea6ef50cd05a4c7d4a5e3f8"}})
-  app.delete("/api/notes", requireLogin, async (req, res) => {
-    await Note.findOneAndDelete({ _user: req.user.id, _id: req.body.noteId });
-    res.send({});
-  });
+  app.patch(
+    "/api/notes/:category",
+    requireLogin,
+    noteController.updateNoteCategory
+  );
+  app.delete("/api/notes", requireLogin, noteController.deleteNote);
 };
