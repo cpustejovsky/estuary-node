1585f643c57de7eddaf45f3929c3f68d6a807e27
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-25 07:51:38 -0400

    refactored requireLogin with simplified arrow function and ternary operator

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,6 +1,4 @@
-module.exports = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  return res.status(401).send({ error: "User must be logged in" });
-};
+module.exports = (req, res, next) =>
+  req.isAuthenticated()
+    ? next()
+    : res.status(401).send({ error: "User must be logged in" });
