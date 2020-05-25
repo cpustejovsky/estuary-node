b0d6de3981f42d8da725c81b008fc107d51b81d4
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-25 07:49:16 -0400

    fixing require Login. sends error message if user is not logged in

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -2,5 +2,5 @@ module.exports = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect("/login");
+  return res.status(401).send({ error: "User must be logged in" });
 };
