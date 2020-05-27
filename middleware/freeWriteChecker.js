2a48f36ca90fce2025152764909f7b1d601c7120
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-27 17:59:29 -0400

    refactor freeWriteChecker wordCount method

--- middleware/freeWriteChecker.js
+++ middleware/freeWriteChecker.js
@@ -1,23 +1,19 @@
 const freeWriteCheckerObject = {
   //TODO: Refine wordCount; It takes every newline as a new character.
   wordCount(string) {
-    let stringArr = string.split(" ");
-    let counter = 0;
-    stringArr.forEach(element => {
-      if (element.length > 0) {
-        counter++;
-      }
-    });
-    let newLineCount = 0;
-    for (let index = 0; index < string.length; index++) {
-      if (string[index] === "\n") newLineCount++;
-    }
-    counter += newLineCount;
-    return counter;
+    return string
+      .split(/\s/)
+      .reduce(
+        (acc, curVal) =>
+          curVal.length > 0 && curVal !== "#n" && curVal !== "#N"
+            ? (acc += 1)
+            : (acc += 0),
+        0
+      );
   },
   noteRemover(freeWrite) {
     let stringArrNL = freeWrite.split("\n");
-    stringArrNL.forEach(element => {
+    stringArrNL.forEach((element) => {
       if (element[0] === "#" && element[1].toString().toUpperCase() === "N") {
         let noteElem = stringArrNL.indexOf(element);
         stringArrNL.splice(noteElem, 1);
@@ -29,12 +25,12 @@ const freeWriteCheckerObject = {
   noteChecker(string) {
     notes = [];
     stringArrNL = string.split("\n");
-    stringArrNL.forEach(element => {
+    stringArrNL.forEach((element) => {
       if (element[0] === "#" && element[1].toString().toUpperCase() === "N") {
         notes.push(element.slice(2));
       }
     });
     return notes;
-  }
+  },
 };
 module.exports = freeWriteCheckerObject;
