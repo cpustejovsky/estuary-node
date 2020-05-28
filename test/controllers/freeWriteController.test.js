056b1158f722b8c105091f446c26b2d06267d4ae
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-28 10:58:23 -0400

    clean up free write controller test

--- test/controllers/freeWriteController.test.js
+++ test/controllers/freeWriteController.test.js
@@ -1,14 +1,14 @@
-const assert = require("assert");
 const mongoose = require("mongoose");
 const chai = require("chai");
+const expect = require("chai").expect;
 const chaiHttp = require("chai-http");
 const app = require("../../app");
 const passportStub = require("passport-stub");
 const User = mongoose.model("users");
 const FreeWrite = mongoose.model("free-writes");
 chai.use(chaiHttp);
-chai.should();
 passportStub.install(app);
+
 const newUser = {
   firstName: "Charles",
   lastName: "Pustejovsky",
@@ -21,22 +21,23 @@ const newFreeWrite = (id) => {
     content: "Free write content",
     date: new Date(),
     _user: id,
-  }
-}
+  };
+};
 describe("Free Write controller", () => {
-  it("GETs /api/free-writes and reads the free-writes", async () => {
-    let savedUser = await new User(newUser).save();
-    let freeWrite = new FreeWrite(newFreeWrite(savedUser._id));
-    let savedFreeWrite = await freeWrite.save();
+  let savedUser;
+  let savedFreeWrite;
+  beforeEach(async () => {
+    savedUser = await new User(newUser).save();
+    savedFreeWrite = await new FreeWrite(newFreeWrite(savedUser._id)).save();
     passportStub.login(savedUser);
+  });
+  it("GETs /api/free-writes and reads the free-writes", async () => {
     let response = await chai.request(app).get("/api/free-writes");
-    assert(response.body[0]._id.toString() === savedFreeWrite._id.toString());
+    expect(response.body[0]._id.toString()).to.equal(
+      savedFreeWrite._id.toString()
+    );
   });
   it("POSTs to /api/free-writes and creates the free-write", async () => {
-    let savedUser = await new User(newUser).save();
-    let freeWrite = new FreeWrite(newFreeWrite(savedUser._id));
-    let savedFreeWrite = await freeWrite.save();
-    passportStub.login(savedUser);
     await chai.request(app).post("/api/free-writes").send({
       title: "Test Free Write #2",
       content: "Free write content #2",
@@ -44,7 +45,7 @@ describe("Free Write controller", () => {
       _user: savedUser._id,
     });
     let freeWrites = await FreeWrite.find({ _user: savedUser._id });
-    assert(freeWrites.length > 1);
+    expect(freeWrites.length).to.be.gt(1)
   });
   // it("DELETES to /api/free-write and destroys the free-write", (done) => {
   //   let user = new User(newUser);
