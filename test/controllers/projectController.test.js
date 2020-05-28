8702e2161e2ed12ac740ec74600c961c4e3cf233
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-28 10:47:11 -0400

    cleaning up project controller tests

--- test/controllers/projectController.test.js
+++ test/controllers/projectController.test.js
@@ -1,6 +1,7 @@
 const assert = require("assert");
 const mongoose = require("mongoose");
 const chai = require("chai");
+const expect = require("chai").expect;
 const chaiHttp = require("chai-http");
 const app = require("../../app");
 const passportStub = require("passport-stub");
@@ -8,7 +9,6 @@ const User = mongoose.model("users");
 const Project = mongoose.model("projects");
 const Note = mongoose.model("notes");
 chai.use(chaiHttp);
-chai.should();
 passportStub.install(app);
 const newUser = {
   firstName: "Charles",
@@ -32,10 +32,14 @@ const newNote = (id) => {
   };
 };
 describe("Project controller", () => {
-  it("POSTs to /api/projects and creates the project", async () => {
-    let savedUser = await new User(newUser).save();
-    await new Project(newProject(savedUser._id)).save();
+  let savedUser;
+  let savedProject;
+  beforeEach(async () => {
+    savedUser = await new User(newUser).save();
+    savedProject = await new Project(newProject(savedUser._id)).save();
     passportStub.login(savedUser);
+  });
+  it("POSTs to /api/projects and creates the project", async () => {
     await chai.request(app).post("/api/projects").send({
       title: "Project Title #2",
       description: "project description #2",
@@ -46,14 +50,10 @@ describe("Project controller", () => {
     assert(projects.length > 1);
   });
   it("GETs /api/projects and reads uncompleted projects", async () => {
-    let savedUser = await new User(newUser).save();
-    let savedProject = await new Project(newProject(savedUser._id)).save();
-    passportStub.login(savedUser);
     let response = await chai.request(app).get("/api/projects");
     assert(response.body[0]._id.toString() === savedProject._id.toString());
   });
   it("GETs /api/projects/done and reads completed projects", async () => {
-    let savedUser = await new User(newUser).save();
     let savedProject = await new Project({
       title: "Project Title",
       description: "project description",
@@ -61,23 +61,16 @@ describe("Project controller", () => {
       _user: savedUser._id,
       completed: true,
     }).save();
-    passportStub.login(savedUser);
     let response = await chai.request(app).get("/api/projects/done");
     assert(response.body[0]._id.toString() === savedProject._id.toString());
   });
   it("GETs /api/projects/show/:id and reads specific project", async () => {
-    let savedUser = await new User(newUser).save();
-    let savedProject = await new Project(newProject(savedUser._id)).save();
-    passportStub.login(savedUser);
     let response = await chai
       .request(app)
       .get(`/api/projects/show/${savedProject._id}`);
     assert(response.body._id.toString() === savedProject._id.toString());
   });
   it("PATCHs to /api/projects and updates the project", async () => {
-    let savedUser = await new User(newUser).save();
-    let savedProject = await new Project(newProject(savedUser._id)).save();
-    passportStub.login(savedUser);
     await chai.request(app).patch("/api/projects").send({
       description: "Project description #2",
       projectId: savedProject._id,
@@ -90,9 +83,6 @@ describe("Project controller", () => {
   });
 
   it("PATCHs to /api/projects/done and marks the project as complete", async () => {
-    let savedUser = await new User(newUser).save();
-    let savedProject = await new Project(newProject(savedUser._id)).save();
-    passportStub.login(savedUser);
     await chai.request(app).patch("/api/projects/done").send({
       projectId: savedProject._id,
     });
@@ -104,10 +94,7 @@ describe("Project controller", () => {
   });
 
   it("PATCHs to /api/projects/done and returns error if project has uncompleted notes", async () => {
-    let savedUser = await new User(newUser).save();
-    let savedProject = await new Project(newProject(savedUser._id)).save();
     let savedNote = await new Note(newNote(savedUser._id)).save();
-    passportStub.login(savedUser);
     await chai
       .request(app)
       .patch("/api/notes/project")
@@ -126,9 +113,6 @@ describe("Project controller", () => {
   });
 
   it("DELETES to /api/projects and destroys the project", async () => {
-    let savedUser = await new User(newUser).save();
-    let savedProject = await new Project(newProject(savedUser._id)).save();
-    passportStub.login(savedUser);
     await chai.request(app).delete(`/api/projects/${savedProject._id}`);
     let project = await Project.findOne({
       _user: savedUser._id,
