37047ff13e6a35d14abdce373f0df783cd5369ec
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-27 20:28:22 -0400

    replace assert with chai expect; beforeEach auth and user creation

--- test/controllers/noteController.test.js
+++ test/controllers/noteController.test.js
@@ -1,6 +1,6 @@
-const assert = require("assert");
 const mongoose = require("mongoose");
 const chai = require("chai");
+const expect = require("chai").expect;
 const chaiHttp = require("chai-http");
 const app = require("../../app");
 const passportStub = require("passport-stub");
@@ -8,7 +8,6 @@ const User = mongoose.model("users");
 const Project = mongoose.model("projects");
 const Note = mongoose.model("notes");
 chai.use(chaiHttp);
-chai.should();
 passportStub.install(app);
 const newUser = {
   firstName: "Charles",
@@ -32,30 +31,32 @@ const newNote = (id) => {
   };
 };
 describe("Note controller", () => {
-  it("POSTs to /api/notes and creates the note", async () => {
-    let savedUser = await new User(newUser).save();
-    await new Note(newNote(savedUser._id)).save();
+  let savedUser;
+
+  beforeEach(async () => {
+    savedUser = await new User(newUser).save();
     passportStub.login(savedUser);
+  });
+
+  it("POSTs to /api/notes and creates the note", async () => {
+    await new Note(newNote(savedUser._id)).save();
     await chai.request(app).post("/api/notes").send({
       content: "Note content #2",
       category: "in-tray",
       _user: savedUser._id,
     });
-    let notes = await Note.find({ _user: savedUser._id, category: "in-tray" });
-    assert(notes.length > 1);
+    expect(
+      await Note.find({ _user: savedUser._id, category: "in-tray" })
+    ).length.to.be.gt(1);
   });
   it("GETs /api/notes/category/:name and reads the notes belonging to that category", async () => {
-    let savedUser = await new User(newUser).save();
     let savedNote = await new Note(newNote(savedUser._id)).save();
-    passportStub.login(savedUser);
     let response = await chai.request(app).get("/api/notes/category/in-tray");
-    assert(response.body[0]._id.toString() === savedNote._id.toString());
+    expect(response.body[0]._id.toString()).to.equal(savedNote._id.toString());
   });
   it("GETs /api/notes/project/:id and reads the notes belonging to that project", async () => {
-    let savedUser = await new User(newUser).save();
     let savedProject = await new Project(newProject(savedUser._id)).save();
     let savedNote = await new Note(newNote(savedUser._id)).save();
-    passportStub.login(savedUser);
     await chai
       .request(app)
       .patch("/api/notes/project")
@@ -63,24 +64,20 @@ describe("Note controller", () => {
     let response = await chai
       .request(app)
       .get(`/api/notes/project/${savedProject._id}`);
-    assert(response.body[0]._id.toString() === savedNote._id.toString());
+    expect(response.body[0]._id.toString()).to.equal(savedNote._id.toString());
   });
   it("PATCHs to /api/notes and updates the note", async () => {
-    let savedUser = await new User(newUser).save();
     let savedNote = await new Note(newNote(savedUser._id)).save();
-    passportStub.login(savedUser);
     await chai.request(app).patch("/api/notes").send({
       content: "Note content #2",
       noteId: savedNote._id,
     });
     let note = await Note.findOne({ _user: savedUser._id, _id: savedNote._id });
-    assert(note.content === "Note content #2");
+    expect(note.content).to.equal("Note content #2");
   });
 
   it("PATCHs to /api/notes/:category and updates the note's category", async () => {
-    let savedUser = await new User(newUser).save();
     let savedNote = await new Note(newNote(savedUser._id)).save();
-    passportStub.login(savedUser);
     await chai.request(app).patch("/api/notes/waiting").send({
       noteId: savedNote._id,
     });
@@ -89,14 +86,12 @@ describe("Note controller", () => {
       _id: savedNote._id,
       category: "waiting",
     });
-    assert(note.category === "waiting");
+    expect(note.category).to.equal("waiting");
   });
 
   it("PATCHs to /api/notes/project and connects the note to the project", async () => {
-    let savedUser = await new User(newUser).save();
     let savedNote = await new Note(newNote(savedUser._id)).save();
     let savedProject = await new Project(newProject(savedUser._id)).save();
-    passportStub.login(savedUser);
     await chai.request(app).patch("/api/notes/project").send({
       noteId: savedNote._id,
       projectId: savedProject._id,
@@ -104,19 +99,18 @@ describe("Note controller", () => {
     let note = await Note.findOne({
       _user: savedUser._id,
       _id: savedNote._id,
-      _project: savedProject._id
+      _project: savedProject._id,
     });
-    assert(note.content === "note content");
+    expect(note.content).to.equal("note content");
+
   });
 
   it("DELETES to /api/notes and destroys the note", async () => {
-    let savedUser = await new User(newUser).save();
     let savedNote = await new Note(newNote(savedUser._id)).save();
-    passportStub.login(savedUser);
     await chai.request(app).delete("/api/notes").send({
       noteId: savedNote._id,
     });
     let note = await Note.findOne({ _user: savedUser._id, _id: savedNote._id });
-    assert(note === null);
+    expect(note).to.equal(null);
   });
 });
