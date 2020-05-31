cf75a7662f90acea717ff0a93593f4624acb0d91
cpustejovsky <charles.pustejovsky@gmail.com>
2020-05-30 20:10:52 -0400

    adding async dropping of all collections before each test to test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 //assignment operator for ES6 Promises
 mongoose.Promise = global.Promise;
 
-before(done => {
+before((done) => {
   mongoose.connect("mongodb://localhost:27017/estuary", {
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -12,15 +12,15 @@ before(done => {
     .once("open", () => {
       done();
     })
-    .on("error", error => {
+    .on("error", (error) => {
       console.warn("Warning", error);
     });
 });
 
 // done does a bit of mocha magic to add asynchronisity
-beforeEach(done => {
-  const { users } = mongoose.connection.collections;
-  users.drop(() => {
-    done();
-  });
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.remove();
+  }
 });
