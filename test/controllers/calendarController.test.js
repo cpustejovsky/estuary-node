4d3aa33299bf764c735539c3b4b5ceb9874008ce
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-02 13:44:15 -0400

    setting up google signin test

--- test/controllers/calendarController.test.js
+++ test/controllers/calendarController.test.js
@@ -1,2 +1,21 @@
-const { google } = require("googleapis");
-const keys = require("../../config/keys");
+// const { google } = require("googleapis");
+// const keys = require("../../config/keys");
+
+const mongoose = require("mongoose");
+const chai = require("chai");
+const expect = require("chai").expect;
+const chaiHttp = require("chai-http");
+const app = require("../../app");
+const User = mongoose.model("users");
+const FreeWrite = mongoose.model("free-writes");
+const Note = mongoose.model("notes");
+const googleLogin = require("../testAutomationHelper");
+chai.use(chaiHttp);
+
+describe("Calendar Controller", function () {
+  this.timeout(5000);
+  it("successfully logs a user in", async () => {
+    const response = await googleLogin();
+    expect(response).to.equal(true);
+  });
+});
