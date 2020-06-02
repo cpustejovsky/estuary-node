b7023cb34e9884f2b7c5720cb9047d0afbef3d47
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-02 13:47:16 -0400

    add page and browser close

--- test/testAutomationHelper.js
+++ test/testAutomationHelper.js
@@ -3,26 +3,28 @@ const keys = require("../config/keys");
 
 module.exports = async function googleLogin() {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto("http://localhost:3000/auth/google");
+    await page.goto("http://localhost:4000/auth/google");
     await page.mainFrame().waitForSelector("#identifierId");
-    console.log("typing email...");
+    // console.log("typing email...");
     await page.type("#identifierId", keys.TEST_GOOGLE_ACCT);
     await page.mainFrame().waitForSelector("#identifierNext");
-    console.log("clicking next button...");
+    // console.log("clicking next button...");
     await page.click("#identifierNext");
-    console.log("waiting for password field...");
+    // console.log("waiting for password field...");
     await page
       .mainFrame()
       .waitForSelector('#password input[type="password"]', { visible: true });
-    console.log("typing password...");
+    // console.log("typing password...");
     await page.type(
       '#password input[type="password"]',
       keys.TEST_GOOGLE_PSSWRD
     );
-    console.log("clicking sign in button...");
-    await page.click("#passwordNext", { delay: 100 });
+    // console.log("clicking sign in button...");
+    await page.click("#passwordNext", { delay: 100 })
+    await page.close()
+    await browser.close()
     return true;
   } catch (error) {
     console.log("something went wrong");
