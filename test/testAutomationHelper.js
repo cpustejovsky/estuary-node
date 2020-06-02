ef06006b766f702f6fa846656ade6265c2799ff6
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-02 12:34:34 -0400

    created login automation

---
+++ test/testAutomationHelper.js
@@ -0,0 +1,32 @@
+const puppeteer = require("puppeteer");
+const keys = require("../config/keys");
+
+module.exports = async function googleLogin() {
+  try {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto("http://localhost:3000/auth/google");
+    await page.mainFrame().waitForSelector("#identifierId");
+    console.log("typing email...");
+    await page.type("#identifierId", keys.TEST_GOOGLE_ACCT);
+    await page.mainFrame().waitForSelector("#identifierNext");
+    console.log("clicking next button...");
+    await page.click("#identifierNext");
+    console.log("waiting for password field...");
+    await page
+      .mainFrame()
+      .waitForSelector('#password input[type="password"]', { visible: true });
+    console.log("typing password...");
+    await page.type(
+      '#password input[type="password"]',
+      keys.TEST_GOOGLE_PSSWRD
+    );
+    console.log("clicking sign in button...");
+    await page.click("#passwordNext", { delay: 100 });
+    return true;
+  } catch (error) {
+    console.log("something went wrong");
+    console.log(error);
+    return false;
+  }
+};
