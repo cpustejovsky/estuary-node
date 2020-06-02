6d7a60b656590b0d4f11f6a1e89baf9e58ba5cb3
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-02 16:23:34 -0400

    got test working as headless browser!!!!

--- test/testAutomationHelper.js
+++ test/testAutomationHelper.js
@@ -1,27 +1,24 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
 const keys = require("../config/keys");
-
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 module.exports = async function googleLogin() {
   try {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto("http://localhost:4000/auth/google");
+    await page.screenshot({ path: "test.png"})
     await page.mainFrame().waitForSelector("#identifierId");
-    // console.log("typing email...");
     await page.type("#identifierId", keys.TEST_GOOGLE_ACCT);
     await page.mainFrame().waitForSelector("#identifierNext");
-    // console.log("clicking next button...");
     await page.click("#identifierNext");
-    // console.log("waiting for password field...");
     await page
       .mainFrame()
       .waitForSelector('#password input[type="password"]', { visible: true });
-    // console.log("typing password...");
     await page.type(
       '#password input[type="password"]',
       keys.TEST_GOOGLE_PSSWRD
     );
-    // console.log("clicking sign in button...");
     await page.click("#passwordNext", { delay: 100 })
     await page.close()
     await browser.close()
