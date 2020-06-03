d6dda8e99699f8a42ae9855259a4ace8b6ef7eb7
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-03 13:36:53 -0400

    setting up mailgun service

---
+++ services/mailgun.js
@@ -0,0 +1,21 @@
+const keys = require("../config/keys")
+const api_key = keys.MAILGUN_API_KEY;
+const domain = "mail.estuaryapp.com";
+const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
+
+//SAMPLE DATA
+// const data = {
+//   from: "Estuary Test <no-reply@estuaryapp.com>",
+//   to: "charles.pustejovsky@gmail.com",
+//   subject: "Howdy! (Test #3",
+//   text: "What hath God wrought?",
+// };
+
+module.exports = async function() {
+  try {
+    let body = await mailgun.messages().send(data)
+    console.log(body)
+  } catch (error) {
+    console.log(error)
+  }
+}
