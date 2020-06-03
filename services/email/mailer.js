26837ff0cc590c6eef4e498fc7dd96c50ac7f977
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-03 17:13:34 -0400

    fetchUsers function

--- services/email/mailer.js
+++ services/email/mailer.js
@@ -5,26 +5,18 @@ const mailgun = require("../mailgun");
 
 module.exports = {
   //fetch all users with email updates as true
-  async fetchEmailUsers(){
-
+  async fetchEmailUsers() {
+    return await User.find({ emailUpdates: true });
   },
   //email intray (daily)
   //for each user, find the notes with category intray, display them in "text", and send them off
-  async emailInTrayNotes(){
-
-  },
+  async emailInTrayNotes() {},
   //email next action items (daily)
   //for each user, find the notes with category intray, display them in "text", and send them off
-  async emailNextActions(){
-
-  },
+  async emailNextActions() {},
   //email ongoing projects (weekly)
   //for each user, find uncompleted projects. display them in text, and send them off
-  async emailProjects(){
-
-  },
+  async emailProjects() {},
   //email completed items within a time period (weekly)
-  async emailCompletedItems(){
-
-  },
+  async emailCompletedItems() {},
 };
