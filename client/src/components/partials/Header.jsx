bf783ba7f0270eda96da87b7f7a38576844bc89d
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-02 20:33:47 -0400

    login and logout included on mobile

--- client/src/components/partials/Header.jsx
+++ client/src/components/partials/Header.jsx
@@ -16,113 +16,117 @@ import {
 
 import MenuIcon from "@material-ui/icons/Menu";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-function MobileMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  return (
-    <div>
-      <MenuIcon
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      />
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem component={RouterLink} to="/about" onClick={handleClose}>
-          About
-        </MenuItem>
-        <MenuItem
-          component={RouterLink}
-          to="/free-writes"
-          onClick={handleClose}
-        >
-          Free Writes
-        </MenuItem>
-        <MenuItem
-          component={RouterLink}
-          to="/notes/in-tray"
-          onClick={handleClose}
-        >
-          Notes
-        </MenuItem>
-        <MenuItem
-          component={RouterLink}
-          to="/projects/list"
-          onClick={handleClose}
-        >
-          Projects
-        </MenuItem>
-      </Menu>
-    </div>
-  );
-}
-
 export default function Header() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
   const classes = useStyles();
-  const auth = useSelector((state) => state.auth);
   const user = useSelector((state) => state.user);
-  const renderAuth = () => {
-    console.log(user)
-    switch (user) {
-      case null:
-        return (
-          <Button style={{ color: "white" }} component={RouterLink} to="/login">
-            Log in
-          </Button>
-        );
-      case false:
-        return (
-          <Button style={{ color: "white" }} component={RouterLink} to="/login">
-            Log in
-          </Button>
-        );
-      default:
+  const MobileMenu = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+    const renderMobileAuth = () => {
+      if (user) {
         return (
           <>
-            <Button
-              style={{ color: "white" }}
-              component={RouterLink}
-              to="/user"
-            >
-              {user
-                ? user.firstName || user.displayName
-                : auth.firstName || auth.displayName}
-            </Button>
-            <Button
-              component={Link}
-              style={{ color: "white" }}
-              underline="none"
-              href="/logout"
-            >
+            <MenuItem component={RouterLink} to="/user">
+              {user.firstName}
+            </MenuItem>
+            <MenuItem component={Link} underline="none" href="/logout">
               Log Out
-            </Button>
+            </MenuItem>
           </>
         );
+      } else {
+        return (
+          <MenuItem component={RouterLink} to="/login">
+            Log in
+          </MenuItem>
+        );
+      }
+    };
+    return (
+      <div>
+        <MenuIcon
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        />
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem component={RouterLink} to="/about" onClick={handleClose}>
+            About
+          </MenuItem>
+          <MenuItem
+            component={RouterLink}
+            to="/free-writes"
+            onClick={handleClose}
+          >
+            Free Writes
+          </MenuItem>
+          <MenuItem
+            component={RouterLink}
+            to="/notes/in-tray"
+            onClick={handleClose}
+          >
+            Notes
+          </MenuItem>
+          <MenuItem
+            component={RouterLink}
+            to="/projects/list"
+            onClick={handleClose}
+          >
+            Projects
+          </MenuItem>
+          <hr></hr>
+          {renderMobileAuth()}
+        </Menu>
+      </div>
+    );
+  }
+  const renderAuth = () => {
+    if (user) {
+      return (
+        <>
+          <Button style={{ color: "white" }} component={RouterLink} to="/user">
+            {user.firstName}
+          </Button>
+          <Button
+            component={Link}
+            style={{ color: "white" }}
+            underline="none"
+            href="/logout"
+          >
+            Log Out
+          </Button>
+        </>
+      );
+    } else {
+      return (
+        <Button style={{ color: "white" }} component={RouterLink} to="/login">
+          Log in
+        </Button>
+      );
     }
   };
   return (
