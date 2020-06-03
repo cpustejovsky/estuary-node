3e0493d1a01eae528150c8febd340c9617e9f7ed
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-03 19:38:40 -0400

    setting up integration test helper for integration tests

--- test/integration/integration_test_helper.js
+++ test/integration/integration_test_helper.js
@@ -1,23 +1,59 @@
 const mongoose = require("mongoose");
 //assignment operator for ES6 Promises
 mongoose.Promise = global.Promise;
+const User = mongoose.model("users");
 
-before(async() => {
+const users = [
+  {
+    firstName: "Charles",
+    lastName: "Pustejovsky",
+    email: "estuaryapptest@gmail.com",
+    emailUpdates: true,
+  },
+  {
+    firstName: "Chas",
+    lastName: "Pustejovsky",
+    email: "estuaryapptest@gmail.com",
+    emailUpdates: true,
+  },
+  {
+    firstName: "Chuck",
+    lastName: "Pustejovsky",
+    email: "estuaryapptest@gmail.com",
+    emailUpdates: false,
+  },
+];
+
+const newInTrayNote = (id) => {
+  return {
+    content: "note content",
+    category: "in-tray",
+    _user: id,
+  };
+};
+
+const newNextAction = (id) => {
+  return {
+    content: "note content",
+    category: "next",
+    _user: id,
+  };
+};
+
+before(async () => {
   mongoose.connect("mongodb://localhost:27017/estuary", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   });
-  await mongoose.connection
-    .on("error", (error) => {
-      console.warn("Warning", error);
-    });
-});
-
-// done does a bit of mocha magic to add asynchronisity
-beforeEach(async () => {
+  await mongoose.connection.on("error", (error) => {
+    console.warn("Warning", error);
+  });
   const collections = await mongoose.connection.db.collections();
   for (let collection of collections) {
     await collection.drop();
+  }
+  for (let user of users) {
+    await new User(user).save();
   }
 });
--- test/integration/mailer.test.js
+++ test/integration/mailer.test.js
@@ -2,29 +2,7 @@ const mongoose = require("mongoose");
 const expect = require("chai").expect;
 const User = mongoose.model("users");
 const mailer = require("../../services/email/mailer")
-const user1 = {
-  firstName: "Charles",
-  lastName: "Pustejovsky",
-  email: "estuaryapptest@gmail.com",
-  emailUpdates: true,
-};
-const user2 = {
-  firstName: "Chas",
-  lastName: "Pustejovsky",
-  email: "estuaryapptest@gmail.com",
-  emailUpdates: true,
-};
-const user3 = {
-  firstName: "Chuck",
-  lastName: "Pustejovsky",
-  email: "estuaryapptest@gmail.com",
-  emailUpdates: false,
-};
-beforeEach(async () => {
-  await new User(user1).save();
-  await new User(user2).save();
-  await new User(user3).save();
-});
+
 describe("Email Methods", async () => {
   it("fetches all users with email updates as true", async () => {
     let allUsers = await User.find();
