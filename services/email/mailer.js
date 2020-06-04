e068be13a9c1ce937bd62a81ee269ecddf062755
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-04 10:50:45 -0400

    adding next action mailer function

--- services/email/mailer.js
+++ services/email/mailer.js
@@ -5,14 +5,11 @@ const mailgun = require("./mailgun");
 const Note = mongoose.model("notes");
 
 module.exports = {
-  //fetch all users with email updates as true
   async fetchEmailUsers() {
     return await User.find({ emailUpdates: true });
   },
-  //email intray (daily)
-  //for each user, find the notes with category intray, display them in "text", and send them off
   async emailInTrayNotes() {
-    let responseMessages = []
+    let responseMessages = [];
     let fetchedUsers = await this.fetchEmailUsers();
     for (const user of fetchedUsers) {
       let userInTrayNotes = await Note.find({
@@ -23,16 +20,34 @@ module.exports = {
         from: "Estuary <no-reply@estuaryapp.com>",
         to: user.email,
         subject: "You Have Notes to Organize",
-        text: userInTrayNotes.map((note) => note.content)
-      }
-      let response = await mailgun(modifiedNotes)
-      responseMessages.push(response.message)
+        text: userInTrayNotes.map((note) => note.content),
+      };
+      let response = await mailgun(modifiedNotes);
+      responseMessages.push(response.message);
     }
-    return responseMessages
+    return responseMessages;
   },
   //email next action items (daily)
   //for each user, find the notes with category intray, display them in "text", and send them off
-  async emailNextActions() {},
+  async emailNextActions() {
+    let responseMessages = [];
+    let fetchedUsers = await this.fetchEmailUsers();
+    for (const user of fetchedUsers) {
+      let userInTrayNotes = await Note.find({
+        category: "next",
+        _user: user._id,
+      });
+      let modifiedNotes = {
+        from: "Estuary <no-reply@estuaryapp.com>",
+        to: user.email,
+        subject: "You Have Next Actions to Complete",
+        text: userInTrayNotes.map((note) => note.content),
+      };
+      let response = await mailgun(modifiedNotes);
+      responseMessages.push(response.message);
+    }
+    return responseMessages;
+  },
   //email ongoing projects (weekly)
   //for each user, find uncompleted projects. display them in text, and send them off
   async emailProjects() {},
