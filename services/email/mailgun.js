c50f4d90b63a469ba3dd1caf57a92b4e834d5d85
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-04 12:04:18 -0400

    sandbox url doesn't work; removing

--- services/email/mailgun.js
+++ services/email/mailgun.js
@@ -1,9 +1,6 @@
 const keys = require("../../config/keys");
 const api_key = keys.MAILGUN_API_KEY;
-const domain =
-  process.env.NODE_ENV === "test"
-    ? "https://api.mailgun.net/v3/sandbox7d3e4535da884a8cbfdf12d162a34c92.mailgun.org"
-    : "mail.estuaryapp.com";
+const domain = "mail.estuaryapp.com";
 const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
 
 //SAMPLE DATA
