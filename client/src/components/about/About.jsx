143b0910cc6ae2ccae535eab00479e18ebab78fc
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-08 17:38:28 -0400

    fixing about view

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -5,48 +5,37 @@ import { Grid, Typography } from "@material-ui/core";
 
 export default function About() {
   return (
-    <Grid container spacing={3} className="site site__about">
-      <Grid item sm></Grid>
-      <Grid item sm={8}>
-        <Typography variant="h4">
-          What is Estuary?
-        </Typography>
-        <p>
-          Thanksgiving weekend 2019, I started reading{" "}
-          <a
-            href="https://www.amazon.com/Getting-Things-Done-Stress-Free-Productivity/dp/0143126563"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Getting Things Done by David Allen
-          </a>
-          . During my plane rides, I had the idea to turn my note taking app,
-          Estuary, into an app that could facilitate the GTD process.
-        </p>
-        <p>
-          That would have the dual benefit of cementing these practices for me
-          and also building a robust app that I can proudly show off in my
-          portfolio.
-        </p>
-        <hr/>
-        <Typography variant="h5">
-        Have any ideas for improvements?
-        </Typography>
-        <p>
-          Here's the source code:{" "}
-          <a href="https://github.com/cpustejovsky/estuary">
-            https://github.com/cpustejovsky/estuary
-          </a>
-          . Feel free to leave a comment or even make a pull request if you're
-          so inclined (check my current list of to-dos first).
-        </p>
-        <Typography variant="h6">
-        Sign up to follow Estuary's progress
-        </Typography>
-        <MailChimpForm />
-        <Todos />
-      </Grid>
-      <Grid item sm></Grid>
-    </Grid>
+    <div className="site site__about">
+      <Typography variant="h4">What is Estuary?</Typography>
+      <p>
+        Thanksgiving weekend 2019, I started reading{" "}
+        <a
+          href="https://www.amazon.com/Getting-Things-Done-Stress-Free-Productivity/dp/0143126563"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Getting Things Done by David Allen
+        </a>
+        . During my plane rides, I had the idea to turn my note taking app,
+        Estuary, into an app that could facilitate the GTD process.
+      </p>
+      <p>
+        That would have the dual benefit of cementing these practices for me and
+        also building a robust app that I can proudly show off in my portfolio.
+      </p>
+      <hr />
+      <Typography variant="h5">Have any ideas for improvements?</Typography>
+      <p>
+        Here's the source code:{" "}
+        <a href="https://github.com/cpustejovsky/estuary">
+          https://github.com/cpustejovsky/estuary
+        </a>
+        . Feel free to leave a comment or even make a pull request if you're so
+        inclined (check my current list of to-dos first).
+      </p>
+      <Typography variant="h6">Sign up to follow Estuary's progress</Typography>
+      <MailChimpForm />
+      <Todos />
+    </div>
   );
 }
