b17f49aa4a7a1a4d801e5fa23f3c9531d60c57c4
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-11 19:30:02 -0400

    consolidating mailers

--- services/email/mailer.js
+++ services/email/mailer.js
@@ -4,76 +4,41 @@ const User = mongoose.model("users");
 const mailgun = require("./mailgun");
 const Note = mongoose.model("notes");
 const Project = mongoose.model("projects");
-
+const dailyEmailTemplate = require("./templates/dailyEmail.js");
 module.exports = {
   async fetchEmailUsers() {
     return await User.find({ emailUpdates: true });
   },
-  async emailInTrayNotes() {
+  async dailyEmailUpdate() {
     let responseMessages = [];
     let fetchedUsers = await this.fetchEmailUsers();
     for (const user of fetchedUsers) {
-      let userInTrayNotes = await Note.find({
+      let inTrayNotes = await Note.find({
         category: "in-tray",
         _user: user._id,
         completed: false,
       });
-      let modifiedNotes = {
-        from: "Estuary <no-reply@estuaryapp.com>",
-        to: user.email,
-        subject: "You Have Notes to Organize",
-        text: userInTrayNotes.map((note) => note.content),
-      };
-      let response = await mailgun(modifiedNotes);
-      responseMessages.push(response.message);
-    }
-    return responseMessages;
-  },
-  async emailNextActions() {
-    let responseMessages = [];
-    let fetchedUsers = await this.fetchEmailUsers();
-    for (const user of fetchedUsers) {
-      let userInTrayNotes = await Note.find({
+      let nextActions = await Note.find({
         category: "next",
         _user: user._id,
         completed: false,
       });
+      let incompleteProjects = await Project.find({
+        _user: user._id,
+        completed: false,
+      });
       let modifiedNotes = {
         from: "Estuary <no-reply@estuaryapp.com>",
         to: user.email,
-        subject: "You Have Next Actions to Complete",
-        text: userInTrayNotes.map((note) => note.content),
+        subject: "Your Daily Update",
+        html: dailyEmailTemplate(inTrayNotes, nextActions, incompleteProjects),
       };
       let response = await mailgun(modifiedNotes);
       responseMessages.push(response.message);
     }
     return responseMessages;
   },
-  //email ongoing projects (weekly)
-  //for each user, find uncompleted projects. display them in text, and send them off
-  async emailProjects() {
-    let responseMessages = [];
-    let fetchedUsers = await this.fetchEmailUsers();
-    for (const user of fetchedUsers) {
-      let incompleteProjects = await Project.find({
-        _user: user._id,
-        completed: false,
-      });
-      let modifiedProjects = {
-        from: "Estuary <no-reply@estuaryapp.com>",
-        to: user.email,
-        subject: `Your Project${incompleteProjects.length > 1 ? "s" : ""}`,
-        text: incompleteProjects.map(
-          (project) => `${project.title} | ${project.description}`
-        ),
-      };
-      let response = await mailgun(modifiedProjects);
-      responseMessages.push(response.message);
-    }
-    return responseMessages;
-  },
-  //email completed items within a time period (weekly)
-  async emailCompletedItems() {
+  async weeklyEmailUpdate() {
     let responseMessages = [];
     let fetchedUsers = await this.fetchEmailUsers();
     for (const user of fetchedUsers) {
@@ -107,5 +72,32 @@ module.exports = {
       responseMessages.push(response.message);
     }
     return responseMessages;
+  },
+  async testEmail() {
+    let testUser = await User.findOne({
+      email: "charles.pustejovsky@gmail.com",
+    });
+    let inTrayNotes = await Note.find({
+      category: "in-tray",
+      _user: testUser._id,
+      completed: false,
+    });
+    let nextActions = await Note.find({
+      category: "next",
+      _user: testUser._id,
+      completed: false,
+    });
+    let incompleteProjects = await Project.find({
+      _user: testUser._id,
+      completed: false,
+    });
+    let modifiedNotes = {
+      from: "Estuary TEST <no-reply@estuaryapp.com>",
+      to: testUser.email,
+      subject: "Test Daily Email",
+      html: dailyEmailTemplate(inTrayNotes, nextActions, incompleteProjects),
+    };
+    let response = await mailgun(modifiedNotes);
+    return response;
   },
 };
