f7c3c8e011c9b235e0f361757e1267926b63812c
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-11 14:13:21 -0400

    creating module; adding testCron to test functionality;

--- services/cron.js
+++ services/cron.js
@@ -1,39 +1,25 @@
 var CronJob = require("cron").CronJob;
-console.log("Before job instantiation");
 
-const logger = () => {
-  console.log("testing with outside function")
-}
-
-const logger2 = () => {
-  console.log("#2 testing with outside function")
-}
-
-const cronJobs = {
-  dailyCron(func) {
-    let job = new CronJob(
-      "*/1 * * * * *",
+module.exports = {
+  testCron(func) {
+    return new CronJob(
+      "* * * * * *",
       func,
       null,
       true,
-      "America/Los_Angeles"
+      "America/New_York"
     );
-    return job
+  },
+  dailyCron(func) {
+    return new CronJob(
+      "00 30 06 */1 * *",
+      func,
+      null,
+      true,
+      "America/New_York"
+    );
   },
   weeklyCron(func) {
-    let job = new CronJob(
-      "*/2 * * * * *",
-      func,
-      null,
-      true,
-      "America/Los_Angeles"
-    );
-    return job
+    return new CronJob("00 30 06 * * 5", func, null, true, "America/New_York");
   },
 };
-
-console.log("After job instantiation");
-
-cronJobs.dailyCron(logger).start()
-cronJobs.weeklyCron(logger2).start()
-
