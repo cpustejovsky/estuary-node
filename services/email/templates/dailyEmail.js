1750518a17042c4d8766358a62b7e2e590e4d6f4
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-11 19:22:01 -0400

    set up and styled dailyEmail template

---
+++ services/email/templates/dailyEmail.js
@@ -0,0 +1,28 @@
+module.exports = dailyEmail = (
+  intray,
+  nextActions,
+  projects
+) => `<h1>Your Daily Update</h1>
+    <h2>Here are your <a href="https://www.estuaryapp.com/notes/next" target="_blank" rel="noopener noreferrer">next actions</a></h2>
+    <ul>
+      ${intray.reduce((acc, note) => (acc += `<li>${note.content}</li>`), ``)}
+    </ul>
+    <hr/>
+    <h2>Here are your <a href="https://www.estuaryapp.com/projects/list" target="_blank" rel="noopener noreferrer">projects</a></h2>
+    <ul>
+      ${projects.reduce(
+        (acc, project) =>
+          (acc += `<a href="https://www.estuaryapp.com/projects/show/${project._id}" target="_blank" rel="noopener noreferrer"><h3>${project.title}</h3></a><p>${project.description}</p>`),
+        ``
+      )}
+    </ul>
+    <hr/>
+    <h2>Here are the note you still need to <a href="https://www.estuaryapp.com/notes/in-tray" target="_blank" rel="noopener noreferrer">organize</a></h2>
+    <ul>
+      ${nextActions.reduce(
+        (acc, note) => (acc += `<li>${note.content}</li>`),
+        ``
+      )}
+    </ul>
+    
+    `;
