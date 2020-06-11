4cfa1b143067d943f34bde5b297b88f5a5289502
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-11 11:13:05 -0400

    test passing

--- test/integration/mailer.test.js
+++ test/integration/mailer.test.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const expect = require("chai").expect;
 const User = mongoose.model("users");
 const Note = mongoose.model("notes");
+const Project = mongoose.model("projects");
 const mailer = require("../../services/email/mailer");
 
 const newInTrayNote = (id, complete) => {
@@ -21,6 +22,17 @@ const newNextAction = (id, complete) => {
     complete: complete ? true : false,
   };
 };
+
+const newProject = (id, complete) => {
+  return {
+    title: "Test Project",
+    description: "project description",
+    dueDate: new Date(),
+    _user: id,
+    completed: complete ? true : false,
+  };
+};
+
 let emailUsers;
 before(async () => {
   emailUsers = await mailer.fetchEmailUsers();
@@ -31,6 +43,10 @@ before(async () => {
     await new Note(newNextAction(user._id)).save();
     await new Note(newNextAction(user._id)).save();
     await new Note(newNextAction(user._id, true)).save();
+
+    await new Project(newProject(user._id)).save();
+    await new Project(newProject(user._id, true)).save();
+    await new Project(newProject(user._id, true)).save();
   }
 });
 describe("Email Methods", async () => {
@@ -56,6 +72,17 @@ describe("Email Methods", async () => {
     let nextNotes = await Note.find({ category: "next" });
     expect(nextNotes.length).to.equal(6);
     let response = await mailer.emailNextActions();
+    expect(response.length).to.equal(2);
+    response.forEach((message) => {
+      expect(message).to.equal("Queued. Thank you.");
+    });
+  });
+  it("for each users, finds the uncompleted Projects", async () => {
+    let foundProjects = await Project.find();
+    expect(foundProjects.length).to.equal(6);
+    let uncompletedProjects = await Project.find({ completed: false });
+    expect(uncompletedProjects.length).to.equal(2);
+    let response = await mailer.emailProjects();
     expect(response.length).to.equal(2);
     response.forEach((message) => {
       expect(message).to.equal("Queued. Thank you.");
