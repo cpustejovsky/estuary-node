083cc63aed1f01a71f2eba3c56bc5f43452c88d9
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-13 17:15:23 -0400

    adding additional code to show the issue

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { connect, useSelector } from "react-redux";
 window.axios = axios;
 
 function Test() {
   const user = useSelector((state) => state.user);
+  const [emailUpdate, setEmailUpdate] = useState(user.emailUpdate)
+  // console.log(user);
   return (
     <div className="center">
       <h1>Test Page</h1>
-      <p>{user.emailUpdates}</p>
+      <p>{user ? user.emailUpdates : ""}</p>
       <p>The axios library is loaded to test routes on this application.</p>
     </div>
   );
