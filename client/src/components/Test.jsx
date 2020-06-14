033ad84aac7d59ec8aa0fda8b846291550183dec
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-14 17:35:14 -0400

    reverting test

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 window.axios = axios;
 
 function Test() {
-  const user = useSelector((state) => state.user);
-  const [emailUpdate, setEmailUpdate] = useState(user.emailUpdate)
-  // console.log(user);
   return (
     <div className="center">
       <h1>Test Page</h1>
-      <p>{user ? user.emailUpdates : ""}</p>
       <p>The axios library is loaded to test routes on this application.</p>
     </div>
   );
