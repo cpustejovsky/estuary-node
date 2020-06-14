ecfee61410268b0cc30b2d4d54a190a4e319924e
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-14 18:26:47 -0400

    setting cron job to 2:00am EST to see if the wrong time is a TZ issue or not

--- services/cron.js
+++ services/cron.js
@@ -6,7 +6,7 @@ module.exports = {
     return new CronJob("0 */1 * * * *", func, null, true, "America/New_York");
   },
   dailyCron(func) {
-    return new CronJob("00 15 05 * * *", func, null, true, "America/New_York");
+    return new CronJob("00 15 02 * * *", func, null, true, "America/New_York");
   },
   weeklyCron(func) {
     return new CronJob("00 30 06 * * 5", func, null, true, "America/New_York");
